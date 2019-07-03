c94a10a901ad1038930ea0d8ba71d8d4b44cf32e
lxxxjd <xjdnihao@163.com>
2019-07-03 19:50:05 +0800

    'xjdnihao'

--- config/router.config.js
+++ config/router.config.js
@@ -19,7 +19,6 @@ export default [
           }
         ],
       },
-
       //newPage
       {
         path: "/Certificate",
---
+++ src/pages/Entrustment/models/form.js
@@ -0,0 +1,29 @@
+import { fakeSubmitForm } from '@/services/api';
+
+export default {
+  namespace: 'Entrustment',
+  form:{
+    data:[
+
+    ]
+  },
+
+  effects: {
+    *submitApplicationForm({ payload }, { call, put }) {
+      yield call(fakeSubmitForm, payload);
+      yield put({
+        type: 'submitApplicationForm',
+        payload,
+      });
+    },
+  },
+
+  reducers: {
+    submitApplicationForm(form, { payload }) {
+      return {
+        ...form,
+        data:payload
+      };
+    },
+  },
+}
