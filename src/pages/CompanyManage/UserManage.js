d96fa257880089b4c9e4ed2c0f486af4a974ae2a
xjdnihao@163.com <xjdnihao@163.com>
2020-02-11 15:22:59 +0800

    'wer'

--- src/pages/CompanyManage/UserManage.js
+++ src/pages/CompanyManage/UserManage.js
@@ -136,6 +136,7 @@ const CreateForm = Form.create()(props => {
             <Option value="业务副总">业务副总</Option>
             <Option value="财务副总">财务副总</Option>
             <Option value="操作经理">操作经理</Option>
+            <Option value="业务经理">实验室主任</Option>
             <Option value="实验室主任">实验室主任</Option>
             <Option value="财务经理">财务经理</Option>
             <Option value="客服人员">客服人员</Option>
